660ad91a3f94a5a41559b38dc3e68ba58e3b3a0a
AJ Sarama <ajsarama@ajs-macbook-pro.myfiosgateway.com>
2023-05-28 15:11:36 -0400

    Added yank to system clipboard keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,10 @@
 -- set <leader> to space
 vim.g.mapleader = " "
+
+-- yank and paste from system register (+)
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>p", '"+p')
+vim.keymap.set("v", "<leader>P", '"+P')
+vim.keymap.set("n", "<leader>p", '"+p')
+vim.keymap.set("n", "<leader>P", '"+P')
